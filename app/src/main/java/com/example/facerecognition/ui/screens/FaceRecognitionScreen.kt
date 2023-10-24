07e56bd98e90cf511d12638a1ecb4ce6046633bd
Betz <silverbez@gmail.com>
2023-10-24 11:36:04 +0300

    pivot to Light sensor data

--- app/src/main/java/com/example/facerecognition/ui/screens/FaceRecognitionScreen.kt
+++ app/src/main/java/com/example/facerecognition/ui/screens/FaceRecognitionScreen.kt
@@ -2,14 +2,12 @@ package com.example.facerecognition.ui.screens
 
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Matrix
-import android.os.Handler
-import android.os.Looper
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.util.Log
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
@@ -17,24 +15,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import com.example.facerecognition.data.model.FileData
+import com.example.facerecognition.utils.MyUtils
 import com.example.facerecognition.presentation.MyViewModel
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.face.Face
-import com.google.mlkit.vision.face.FaceDetection
-import com.google.mlkit.vision.face.FaceDetector
-import com.google.mlkit.vision.face.FaceDetectorOptions
-import java.io.File
-import java.util.concurrent.Executors
-import java.util.concurrent.atomic.AtomicInteger
+import com.example.facerecognition.ui.navigation.Screen
 
 @Composable
 fun FaceRecognitionScreen(
@@ -42,44 +38,54 @@ fun FaceRecognitionScreen(
     myViewModel: MyViewModel
 ) {
     val context = LocalContext.current
-    val cameraExecutor = rememberCoroutineScope()
 
     val controller = remember {
         LifecycleCameraController(context).apply {
-            setEnabledUseCases(CameraController.IMAGE_CAPTURE)
+            setEnabledUseCases(CameraController.IMAGE_CAPTURE or CameraController.VIDEO_CAPTURE)
         }
     }
     controller.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
 
     // State to track the number of photos taken
-    var photosTaken by remember { mutableStateOf(0) }
+    var taken by remember { mutableStateOf(0) }
 
-    // Set up the image capture use case
-    val imageCapture = remember {
-        ImageCapture.Builder()
-            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-            .build()
-    }
-    val numPhotosToCapture = 30
-    val interval = 1000L
 
-    // Start the camera capture session when the screen is first composed
-    LaunchedEffect(Unit) {
-        val handler = Handler(Looper.getMainLooper())
-        val photosTaken = AtomicInteger(0)
+    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
-        val runnable = object : Runnable {
-            override fun run() {
-                if (photosTaken.get() < numPhotosToCapture) {
-                    captureImage(context, controller, imageCapture) { capturedBitmap ->
-                        runFaceContourDetection(capturedBitmap)
-                    }
-                    photosTaken.incrementAndGet()
-                    handler.postDelayed(this, interval)
+    DisposableEffect(lightSensor) {
+        val listener = object : SensorEventListener {
+            private var lastEventTime = 0L
+
+            override fun onSensorChanged(event: SensorEvent?) {
+                // Get the current time
+                val currentTime = System.currentTimeMillis()
+
+                // Check if the last event was more than 1000 ms ago
+                if (currentTime - lastEventTime >= 1000L) {
+                    // Get the light sensor Lux value
+                    val lux = event?.values?.get(0) ?: 0f
+
+
+                    lastEventTime = currentTime
+                    // Do something with the light sensor Lux value
+                    Log.d("zzzzz", lux.toString())
+
+                    FileData(currentTime.toString(), MyUtils.isLightInRange(lux))
                 }
+
             }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            }
+
         }
-        handler.postDelayed(runnable, interval)
+
+        sensorManager.registerListener(listener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+        onDispose {
+            sensorManager.unregisterListener(listener)
+        }
     }
 
     Box(
@@ -94,8 +100,10 @@ fun FaceRecognitionScreen(
         // Your UI elements, and a "Complete Session" button
         Button(
             onClick = {
+                navController.navigate(Screen.SessionSummary.route) {
+                    popUpTo(Screen.SessionSummary.route) { inclusive = true }
+                }
                 myViewModel.onCameraSessionCompleted()
-                // Add navigation or other actions
             },
             modifier = Modifier.align(Alignment.BottomCenter)
         ) {
@@ -122,51 +130,4 @@ fun CameraPreview(
     )
 }
 
-fun captureImage(
-    context: Context,
-    controller: LifecycleCameraController,
-    imageCapture: ImageCapture,
-    onPhotoTaken: (Bitmap) -> Unit
-) {
-    val outputFile = File(context.cacheDir, "temp_image.jpg")
-    val outputOptions = ImageCapture.OutputFileOptions.Builder(outputFile).build()
-    imageCapture.takePicture(
-        outputOptions,
-        Executors.newSingleThreadExecutor(),
-        object : ImageCapture.OnImageSavedCallback {
-            override fun onError(exc: ImageCaptureException) {
-                // Handle image capture error
-                exc.printStackTrace()
-            }
 
-            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                val savedImageFile = output.savedUri
-                val bitmap = BitmapFactory.decodeFile(savedImageFile?.path)
-                onPhotoTaken(bitmap)
-            }
-        }
-    )
-}
-
-fun runFaceContourDetection(bmp: Bitmap) {
-    val image = InputImage.fromBitmap(bmp, 0)
-    val options = FaceDetectorOptions.Builder()
-        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
-        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
-        .build()
-    val detector: FaceDetector = FaceDetection.getClient(options)
-    detector.process(image)
-        .addOnSuccessListener { faces ->
-            processFaceContourDetectionResult(faces)
-        }
-        .addOnFailureListener(
-            OnFailureListener { e ->
-                e.printStackTrace()
-            })
-}
-
-fun processFaceContourDetectionResult(faces: List<Face>) {
-    if (faces.isEmpty()) {
-        // Handle no face found
-    }
-}
---
+++ app/src/main/java/com/example/facerecognition/utils/MyUtils.kt
@@ -0,0 +1,16 @@
+package com.example.facerecognition.utils
+
+class MyUtils {
+
+
+    companion object {
+
+        private const val MIN_LIGHT = 20f
+        private const val MAX_LIGHT = 1000f
+
+        fun isLightInRange(lux: Float): Boolean {
+            return lux in MIN_LIGHT..MAX_LIGHT
+        }
+    }
+
+}
